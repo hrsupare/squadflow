f4649b4f7576c95ac9e5a9914591301637411538
hrsupare <hsupar313@gmail.com>
2023-12-12 06:45:59 +0530

    Add :: add logo for navbar and footer

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,9 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import localFont from "next/font/local";
-
-import { cn } from "@/lib/utils";
-
+ 
 const headingFont = localFont({
   src: "../public/fonts/font.woff2",
 });
@@ -15,15 +13,9 @@ export const Logo = () => {
         <Image
           src="/logo.svg"
           alt="Logo"
-          height={30}
-          width={30}
+          height={160}
+          width={190}
         />
-        <p className={cn(
-          "text-lg text-neutral-700 pb-1",
-          headingFont.className,
-        )}>
-          SquadFlow
-        </p>
       </div>
     </Link>
   );
